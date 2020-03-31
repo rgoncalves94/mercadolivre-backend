fef8f763fef98f4073deb4102712115933c7e40f
Rafael Gonçalves <r.goncalves94@hotmail.com>
2020-03-30 23:12:02 -0300

    creating service to call MercadoLivre's apis

---
+++ src/services/MercadoLivreProductsClientService/index.js
@@ -0,0 +1,50 @@
+const fetch = require('node-fetch')
+const { MERCADOLIVRE_API_BASEURL } = process.env
+const { MercadoLivreError, StandardError } = require('../../errors')
+
+/**
+ * Call MELI's product search API
+ * @param {*} term 
+ * @return {object}
+ */
+async function searchProducts(term) {
+   return _callGetAPI(`${MERCADOLIVRE_API_BASEURL}/sites/MLA/search?q=${term}`)
+}
+
+/**
+ * Call MELI's product detail API
+ * @param {number} idProduct 
+ * @return {object}
+ */
+function detailProduct(idProduct) {
+    return _callGetAPI(`${MERCADOLIVRE_API_BASEURL}/items/${idProduct}`)
+}
+
+/**
+ * Call MELI's product description API
+ * @param {number} idProduct 
+ * @return {object}
+ */
+function descriptionProduct(idProduct) {
+    return _callGetAPI(`${MERCADOLIVRE_API_BASEURL}/items/${idProduct}/description`)
+}
+
+/**
+ * Call GET APis
+ * @param {string} URL
+ * @return {object}
+ */
+async function _callGetAPI(URL) {
+    try {
+        const response = await fetch(URL)
+        const data = await response.json()
+
+        if(!response.ok)
+            throw new MercadoLivreError(data)
+        
+        return data;
+    } catch(error) {
+        throw new StandardError(500, "Internal Server Error")
+    }
+}
+module.exports = { searchProducts, detailProduct, descriptionProduct }
